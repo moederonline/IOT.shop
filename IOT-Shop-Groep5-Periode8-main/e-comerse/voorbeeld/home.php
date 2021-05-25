3bb8f2247c54a887e21d27af20b5890d22f78ab9
Fenno1 <61690684+Fenno1@users.noreply.github.com>
2021-05-25 13:20:00 +0200

    Add files via upload

---
+++ IOT-Shop-Groep5-Periode8-main/e-comerse/voorbeeld/home.php
@@ -0,0 +1,84 @@
+
+<!doctype html>
+<html>
+<head>
+    <title>Devwear</title>
+    <meta charset="utf-8">
+    <meta name="description" content="Clothing webshop for nerdy developing types. Good quality products.">
+    <meta name="keywords" content="Devwear, webshop, Hoodie, T-shirt, Caps, Developers, Clothing">
+    <meta name="author" content="Fenno Elzinga">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="../CSS/style.css" rel="stylesheet" type="text/css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <!-- Header -->
+        <div class="header">
+            <h1>Blogsite</h1>
+            <p>Kleding voor een <b>echte</b> programmeur</p>
+        </div>
+
+        <!-- Nav -->
+        <div class="navbar">
+            <div class="img-nav">
+                <a href="#" style="margin: 50px;"><img id="logo_devwear" src="../afbeeldingen/devwear_logo.png"></a>
+            </div>
+            <a class="links-nav" href="#">Webshop</a>
+            <a class="links-nav" href="#">Pagina 2</a>
+            <a class="links-nav" href="#">Pagina 3</a>
+        </div>
+    </header>
+
+    <main>
+        <section class="flex-row">
+            <article class="sidebar">
+                <h2>Wie zijn wij?</h2>
+                <p style="color: #1abc9c;;"><b>Ons logo:</b></p>
+                <img src="../afbeeldingen/devwear_logo.png">
+                <p>Wij zijn een jong bedrijf die kleding aanbied aan 'Nerdy' programmeurs. Een aantal
+                    maanden geleden wouden wij graag iets met kleding doen tegelijkertijd met programmeren.
+                    Uiteindelijk kwamen we hierop uit! Ons assortiment is erg groot en varierend.</p>
+                <h3><b>Voorbeelden</b></h3>
+                <p style="color: #1abc9c;"><b>Hieronder ziet u een aantal designs:</b></p>
+                <div class="test">
+                    <img class="design-img" src="../afbeeldingen/kleding3.jpg"><br>
+                    <img class="design-img" src="../afbeeldingen/kleding4.jpg"><br>
+                    <img class="design-img" src="../afbeeldingen/kleding5.jpg">
+                </div>
+
+            </article>
+
+            <section class="main-content">
+                <h1>Devwear maakt <b>JOU</b></h1>
+                <p>Wij bij Devwear staan voor jou belangen!</p>
+                <img id="main-kleding1" src="../afbeeldingen/kleding.jpg">
+                <p style="width: 800px;">Alles wat wij verkopen is Fairtrade. Wij doen ons uiterste best om zo duurzaam
+                    mogelijk te zijn met
+                    onze producten. Twijfel niet en ga direct naar onze <a
+                        style="text-decoration: none; color: #1abc9c;" href="#"><b>webshop</b></a> om ons hele
+                    assortiment te bekijken. </p>
+
+                <h1>Niet overtuigd?</h1>
+                <p>Nog een van onze kledingstukken:</p>
+                <img id="main-kleding2" src="../afbeeldingen/kleding2.jpg">
+                <p style=" width: 800px;">Wij doen er alles aan om onze klant tevreden te houden! Bent u nog niet
+                    zeker van uw zaak of heeft u
+                    een vraag over onze service? Neem direct contact op met
+                    onze
+                    klantenservice.<br><b style="color:#1abc9c;">- devwear@gmail.com</b><br><b style="color:#1abc9c;">-
+                        070 405 3876</b></p>
+
+            </section>
+        </section>
+    </main>
+
+    <footer>
+        <div class="footer">
+            <h2>Disclaimer @2021 Devwear</h2>
+        </div>
+    </footer>
+
+</body>
+</html>
